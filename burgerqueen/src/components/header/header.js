44b146e9766e5b725fce51e9ab51021400ff55f6
laurapalma <lau.palma9617@gmail.com>
2021-10-14 12:12:30 -0500

    Maquetación Header y estilos SCSS

--- burgerqueen/src/components/header/header.js
+++ burgerqueen/src/components/header/header.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import letras from './img/letras.png'
+import burguer from './img/burguer.png'
+import dish from './img/dish 1.png'
+import chef from './img/chef.png'
+import './header.scss'
 
 const Header = (props) => {
     return(
-        <header className='header'>Burger Queen</header>   
+        <header>
+           <img className='letters' src={letras} alt='burguerqueen'></img>
+           <img className='burguer' src={burguer} alt='burguerqueen'></img>
+           <h2 className='hours'>24 hrs</h2>
+           <img className='dish' src={dish} alt='burguerqueen'></img>
+           <h4 className='waiter'>Modo mesero</h4>
+           <img className='chef' src={chef} alt='burguerqueen'></img>
+           <h4 className='kitchen'>Modo cocina</h4>
+        </header>   
     )
 }
 
