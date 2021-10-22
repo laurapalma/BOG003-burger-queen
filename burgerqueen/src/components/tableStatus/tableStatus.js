928cc2dc61055b8bc8b8f5ab5d0a68b94b0760cc
Gleysi Ascanio <gleysiascanio29@gmail.com>
2021-10-22 13:29:29 -0500

    Continuacion de estilos, mejoras en la pantalla de mesero

--- burgerqueen/src/components/tableStatus/tableStatus.js
+++ burgerqueen/src/components/tableStatus/tableStatus.js
@@ -4,7 +4,18 @@ import './tableStatus.scss'
 const Tablestatus = (props) => {
     return(
         <div className='tablesContainer'>
-            <h4 className='status'>Aqui se veran las mesas</h4>   
+            <h3>Mesa 1</h3>
+                <h4> Disponible </h4>
+            <h3>Mesa 2</h3>
+                <h4> Enviado </h4> 
+            <h3>Mesa 3</h3> 
+                <h4> En Proceso </h4>
+            <h3>Mesa 4</h3> 
+                <h4> Pedido Listo </h4>
+            <h3>Mesa 5</h3> 
+                <h4> Enviado </h4>
+            <h3>Mesa 6</h3>      
+                <h4> Entregado </h4>
         </div>        
     )
 }
