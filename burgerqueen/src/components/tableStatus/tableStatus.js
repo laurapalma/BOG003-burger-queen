14ffe8104829380b607441bd0d4b436e7212f2fb
laurapalma <lau.palma9617@gmail.com>
2021-11-02 18:27:11 -0500

    Se renderiza pedidos en modo mesero

--- burgerqueen/src/components/tableStatus/tableStatus.js
+++ burgerqueen/src/components/tableStatus/tableStatus.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './tableStatus.scss'
+import { onSnapshot, collection } from "firebase/firestore";
+import db  from "../firebase/firebaseConfig"
 
 const Tablestatus = (props) => {
-    return(
-        <div className='tablesContainer'>
-            <h3>Mesa 1</h3>
-                <h4> Disponible </h4>
-            <h3>Mesa 2</h3>
-                <h4> Enviado </h4> 
-            <h3>Mesa 3</h3> 
-                <h4> En Proceso </h4>
-            <h3>Mesa 4</h3> 
-                <h4> Pedido Listo </h4>
-            <h3>Mesa 5</h3> 
-                <h4> Enviado </h4>
-            <h3>Mesa 6</h3>      
-                <h4> Entregado </h4>
-        </div>        
-    )
-}
-
+        const [table, setTable] = useState()
+        
+        useEffect(() => {         
+            const callOrders = () => { 
+                onSnapshot(collection(db, "orders"), (querySnapshot) => {
+                    let clients = []
+                        querySnapshot.forEach((doc) => {
+                        clients.push({...doc.data(), id: doc.id});
+                    });
+                   console.log(clients); 
+                   setTable(clients);    
+                });           
+            }
+            callOrders()
+        }, []); 
+        console.log(table);
+        return(
+            <>
+                <div className='tablesContainer'>
+                    {table && table.map(item => 
+                        <div key={item.id} className='state'>
+                            <h3>Mesa {item.table.table}</h3>                            
+                            <h4>{item.state}</h4>                 
+                        </div>
+                    )}
+                </div>
+            </>  
+        )
+    }
 export default Tablestatus
