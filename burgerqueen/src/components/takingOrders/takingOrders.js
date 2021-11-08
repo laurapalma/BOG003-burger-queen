25587e3226175d90f41641d9387496c0d16838c6
Gleysi Ascanio <gleysiascanio29@gmail.com>
2021-11-08 08:37:10 -0500

    Se agregan comentarios al código

--- burgerqueen/src/components/takingOrders/takingOrders.js
+++ burgerqueen/src/components/takingOrders/takingOrders.js
@@ -7,7 +7,7 @@ import '../modal/modal.scss'
 
 
 
-const Takingorders = ({initialState, state, menuData, menuState, on_change, handleInputChange, prices, comment, handleCleaner}) => {
+const Takingorders = ({state, menuData, menuState, on_change, handleInputChange, prices, comment, handleCleaner}) => {
 
     let text=[], subtotal=[], cant, name, table, quants 
         let message = document.getElementById('message')
@@ -25,16 +25,17 @@ const Takingorders = ({initialState, state, menuData, menuState, on_change, hand
                 }
             }
         }
-
-    const [isOpenModal, openModal, closeModal] = useModal(false); 
- 
-    const validationOrder =() => {
-        if (message === null) {
+        // Se declara la modal en False, para que no se muestre.
+        const [isOpenModal, openModal, closeModal] = useModal(false); 
+        
+        // se crea condicionales para que se valide si se llenaron todos los campos de orden y se abra la modal.
+        const validationOrder =() => {
+            if (message === null) {
             alert('No has hecho un pedido')
             handleCleaner();        
-        }else if (table === '' || name === ''){
+            }else if (table === '' || name === ''){
             message.innerHTML= `Por favor rellene todos los campos`        
-        } else {
+            } else {
             openModal()
         }
     }
@@ -43,6 +44,7 @@ const Takingorders = ({initialState, state, menuData, menuState, on_change, hand
         <>
         <section>
                 <div className="infoTable">
+                    {/* Evento del select, toma el nombre y el valor de la mesa. */}
                     <select id='selectTable' onChange= {e => handleInputChange(e)} name='table' value = {state.table}>
                         <option value='0'>Mesas</option>
                         <option value='1'>Mesa 1</option>
@@ -52,6 +54,7 @@ const Takingorders = ({initialState, state, menuData, menuState, on_change, hand
                         <option value='5'>Mesa 5</option>
                         <option value='6'>Mesa 6</option>
                     </select>
+                    {/* Se toma el valor del nombre del cliente */}
                     <input 
                         className="nameUser"
                         id='nameUser'
@@ -63,9 +66,9 @@ const Takingorders = ({initialState, state, menuData, menuState, on_change, hand
                     />
                 </div>
                 <p id='message' className='message'></p>
+                {/* Se recorre la data por cada key y se traen los valores de cada producto del menú*/}
                 {menuData.map((item, i) => 
                 <div key={i} className='orderContainer'>
-                    <h6>{item.producto}</h6>
                     <input 
                             type="number" 
                             id={item["key"] + "_id"} 
@@ -75,6 +78,8 @@ const Takingorders = ({initialState, state, menuData, menuState, on_change, hand
                             min="0"
                             onChange = {on_change}
                     />
+                    <h6>{item.producto}</h6>
+                        {/* Se multiplica el valor de la key actual de ese producto por el precio almacenado en la data*/}
                     <p> $ {parseInt(menuState[item["key"]])*parseInt(item["precio"])}</p>
                 </div>
                 )}
@@ -88,7 +93,10 @@ const Takingorders = ({initialState, state, menuData, menuState, on_change, hand
                 <button className="btnOrders" onClick = { () =>{ validationOrder(); } }>Enviar</button>
                 </div>
         </section> 
+        {/*Se le envia a Modal las props de abrir y cerrar modal*/}
         <Modal isOpen ={isOpenModal} closeModal={closeModal}>
+            {/*Se le envia a ModalContent las props de todos los datos de la orden, cerrar la modal, 
+            limpiar todos los campos*/}
                     <ModalContent
                     state={state}
                     table={table}
